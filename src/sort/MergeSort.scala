9ab770f492bc98a1bf452fda0b4b959ec31521d1
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-01-26 22:42:25 +0700

    removed Maven's files

---
+++ src/sort/MergeSort.scala
@@ -0,0 +1,39 @@
+/**
+ * This file is part fo Scalacaster project, https://github.com/vkostyukov/scalacaster
+ * and written by Vladimir Kostyukov, http://vkostyukov.ru
+ */
+
+def mergesort(input: Array[Int]): Array[Int] = {
+  if (input.length == 1) {
+    return input
+  }
+
+  val n = input.length / 2
+
+  val left = mergesort(input.slice(0, n))
+  val right = mergesort(input.slice(n, input.length))
+
+  var result: Array[Int] = new Array(input.length)
+
+  var i = 0
+  var j = 0
+  result.indices foreach { k =>
+    if (i == left.length) {
+      result(k) = right(j)
+      j += 1
+    } else if (j == right.length) {
+      result(k) = left(i)
+      i += 1
+    } else if (left(i) > right(j)) {
+      result(k) = right(j)
+      j += 1
+    } else {
+      result(k) = left(i)
+      i += 1
+    }
+  }
+
+  return result
+}
+
+assert { mergesort(Array(5, 2, 1, 3, 4)).deep == Array(1, 2, 3, 4, 5).deep }
