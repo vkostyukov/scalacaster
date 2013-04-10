f6a173b55ae14af9bffb119b331a64acbbe885f6
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-04-11 00:25:14 +0700

    added some List methods

--- src/collection/List.scala
+++ src/collection/List.scala
@@ -16,23 +16,50 @@ abstract class List[+A] {
   def head: A
   def tail: List[A]
 
+  def isEmpty: Boolean
+
   def append[B >: A](x: B): List[B] =
-    if (isEmpty) new Cons(v) 
+    if (isEmpty) new Cons(x) 
     else new Cons(head, tail.append(x))
 
   def prepend[B >: A](x: B): List[B] = 
     new Cons(x, this)
 
+  def remove[B >: A](x: B): List[B] = 
+    if (isEmpty) throw new NoSuchElementException("Can't find " + x + " in this list.")
+    else if (x != head) new Cons(head, tail.remove(x))
+    else tail
+
   def apply(n: Int): A = {
-    def loop(l: List[A], m: Int): List[A] = 
+    def loop(l: List[A], m: Int): A = 
       if (l.isEmpty) throw new NoSuchElementException("Index out of the bounds.")
-      else if (m == 0) l
+      else if (m == 0) l.head
       else loop(l.tail, m - 1)
 
     loop(this, n)
   }
 
-  def isEmpty: Boolean
+  def contains[B >: A](x: B): Boolean = 
+    if (isEmpty) false
+    else if (x != head) tail.contains(x)
+    else true
+
+  def foreach(f: (A) => Unit): Unit =
+    if (!isEmpty) {
+      f(head)
+      tail.foreach(f)
+    }
+
+  def fold[B](n: B)(op: (B, A) => B): B = {
+    def loop(l: List[A], a: B): B =
+      if (l.isEmpty) a
+      else loop(l.tail, op(a, l.head))
+
+    loop(this, n)
+  }
+
+  def sum[B >: A](implicit num: Numeric[B]): B = fold(num.zero)(num.plus)
+  def product[B >: A](implicit num: Numeric[B]): B = fold(num.one)(num.times)
 
   def reverse: List[A] = { 
     def loop(s: List[A], d: List[A]): List[A] = 
@@ -45,6 +72,15 @@ abstract class List[+A] {
   def length: Int = 
     if (isEmpty) 0 
     else 1 + tail.length
+
+  override def toString: String = {
+    def loop(h: A, t: List[A], s: String): String = 
+      if (!t.isEmpty) loop(t.head, t.tail, s + h + ", ")
+      else s + h
+
+    if (isEmpty) "List[]"
+    else "List[" + loop(head, tail, "") + "]"
+  }
 }
 
 object Nill extends List[Nothing] { // since 'Nil' already reserved 
@@ -64,7 +100,10 @@ class Cons[A](h: A, t: List[A] = Nill) extends List[A] {
 object List {
   def apply[A](xs: A*): List[A] = {
     var r: List[A] = Nill
-    for (x <- xs.reverse) r = r <+ x
+    for (x <- xs.reverse) r = r.prepend(x)
     r
   }
 }
+
+var l = List(1, 2, 3, 4, 5)
+println(l)
