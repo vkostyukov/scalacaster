e688bcb08ba5e28cf931b2e8c7f120996885b510
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-06-13 17:12:43 +0700

    added aletrnative implementations for linear search

--- src/search/LinearSearch.scala
+++ src/search/LinearSearch.scala
@@ -9,11 +9,34 @@
  * Average - O(n)
  */
 
-def linearsearch[A](list: List[A], key: A): A = {
-  def loop(as: List[A]): List[A] = 
-    if (as.isEmpty) Nil
+def linearsearchCond[A](list: List[A], key: A): A = {
+  def loop(as: List[A]): A = 
+    if (as.isEmpty) null.asInstanceOf[A]
     else if (as.head == key) as.head
     else loop(as.tail)
 
   loop(list)
+}
+
+def linearsearchPM[A](list: List[A], key: A): A = {
+  def loop(as: List[A]): A = as match {
+    case h :: t =>
+      if (h == key) h
+      else loop(t)
+    case Nil => null.asInstanceOf[A]
+  } 
+
+  loop(list)
+}
+
+def linearsearchIter[A](list: List[A], key: A): A = {
+  var that: List[A] = list
+  var r: A = null.asInstanceOf[A]
+
+  while (!that.isEmpty && r != null) {
+    if (that.head == key) r = that.head
+    that = that.tail
+  }
+
+  r
 }
