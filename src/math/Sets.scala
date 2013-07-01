fd57cff1d90fb9346bbb7487f4e39786e8bc813d
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-07-01 11:15:02 +0700

    added new subsets implementation

--- src/math/Sets.scala
+++ src/math/Sets.scala
@@ -9,12 +9,36 @@ object Sets {
   /**
    * Generates all subsets of given set 's'.
    *
+   * NOTES: This method uses Scala Style.
+   *
    * Time - O()
    * Space - O()
    */
-  def subsets[A](s: List[A]): List[List[A]] = s match {
+  def subsetsScala[A](s: List[A]): List[List[A]] = s match {
     case a :: as => val ss = subsets(as); List(a) :: ss ::: ss.map((x: List[A]) => a :: x)
     case Nil => Nil
+  }
+
+  /**
+   * Generates all subsets of given set 's'.
+   *
+   * NOTES: This method uses purely functional style.
+   *
+   * Time - O()
+   * Space - O()
+   */
+  def subsetsFunc[A](s: List[A]): List[List[A]] = {
+    def loop(ss: List[A]): List[List[A]] = ss match {
+      case a :: as => merge(a, loop(as), Nil)
+      case Nil => Nil
+    }
+
+    def merge(a: A, as: List[List[A]], bs: List[List[A]]): List[List[A]] = as match {
+      case c :: cs => merge(a, cs, (a :: c) :: c :: bs)
+      case Nil => List(a) :: bs
+    }
+
+    loop(s)
   }
 
   /**
