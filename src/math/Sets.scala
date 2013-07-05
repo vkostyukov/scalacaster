9bf216b0b707888996661d63ca617a702061c4b7
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-07-05 10:54:41 +0700

    cleared names a bit

--- src/math/Sets.scala
+++ src/math/Sets.scala
@@ -7,22 +7,42 @@
 object Sets {
 
   /**
-   * Generates all combinations of given set 's' with given length 'n'.
+   * Generates combinations of given set 's' with given length 'k'.
+   * 
+   * NOTES: To count number of combinations the following low can be used:
    *
-   * Time - O()
-   * Space - O()
+   * C_k,n = n!/(k!(n - k)!
+   *
+   * Time - O(C_k,n)
+   * Space - O(C_k,n)
    */
   def combinations[A](s: List[A], k: Int): List[List[A]] = 
     if (k > s.length) Nil
     else if (k == 1) s.map(List(_))
     else combinations(s.tail, k - 1).map(s.head :: _) ::: combinations(s.tail, k)
 
-  /**
-   * Generates all combinations of given set 's'.
-   *
-   * Time - O()
-   * Space - O()
-   */
-   def combinations[A](s: List[A]): List[List[A]] = 
-     (2 to s.length).foldLeft(combinations(s, 1))((a, i) => combinations(s, i) ::: a)
+   /**
+    * Generates variations of given set 's' with given length 'k'.
+    * 
+    * NOTES: To count number of variations the following low can be used:
+    * 
+    * V_k,n = n!/(n - k)!
+    *
+    * Time - O(V_k,n)
+    * Space - O(V_k,n)
+    */
+   def variations(s: List[A], k: Int): List[List[A]] = ???
+
+   /**
+    * Generates all permutations of given set 's'.
+    *
+    * NOTES: To count number of permutations the following low can be used:
+    *
+    * P_n = V_n,n = n!
+    *
+    * Time - O(n!)
+    * Space - O(n!)
+    */
+   def permutations[A](s: List[A]): List[List[A]] = 
+     (2 to s.length).foldLeft(variations(s, 1))((a, i) => variations(s, i) ::: a)
 }
