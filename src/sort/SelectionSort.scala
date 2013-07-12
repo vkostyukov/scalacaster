8a078f0b565bc8c949372f5a7846a2a69f8d2f9b
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-07-12 17:14:39 +0700

    rewrote SelectionSort

--- src/sort/SelectionSort.scala
+++ src/sort/SelectionSort.scala
@@ -11,12 +11,17 @@
 
 def selectionsort[A <% Ordered[A]](list: List[A]): List[A] = {
   def sort(as: List[A], bs: List[A]): List[A] = as match {
-    case h :: t => select(as.max, as, bs)
+    case h :: t => select(h, t, Nil, bs)
     case Nil => bs
   }
 
-  def select(m: A, as: List[A], bs: List[A]): List[A] = 
-    sort(as.filter(_ != m), m :: bs)
+  def select(m: A, as: List[A], zs: List[A], bs: List[A]): List[A] = 
+    as match {
+      case h :: t => 
+        if (m > h) select(m, t, h :: zs, bs)
+        else select(h, t, m :: zs, bs)
+      case Nil => sort(zs, m :: bs)
+    }
 
   sort(list, Nil)
 }
