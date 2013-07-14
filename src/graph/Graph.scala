a5412b2826bca3238c232aec15a2d3c0d58aaa0e
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2013-07-14 22:44:55 +0700

    added dijkstra

--- src/graph/Graph.scala
+++ src/graph/Graph.scala
@@ -129,17 +129,7 @@ class Graph[E, N](var value: N = null.asInstanceOf[N]) {
    * Time - O()
    * Space - O()
    */
-  def nodes: Int = graphsByDepth.size
-
-  /**
-   * Returns the number of edges in this graph.
-   *
-   * Time - O()
-   * Space - O()
-   */
-  def edges: Int = {
-
-  }
+  def size: Int = graphsByDepth.size
 
   override def equals(o: Any): Boolean = o match {
     case g: Graph[_, _] => g.value == value
@@ -152,13 +142,46 @@ class Graph[E, N](var value: N = null.asInstanceOf[N]) {
 class WeightedGraph[N](n: N) extends Graph[Double, N](n) {
 
   /**
-   * Searches for the shortest path in graph.
+   * Searches for the shortest path in this graph.
    *
    * Time - O()
    * Space - O()
    */
   def dijkstra(from: N, to: N): List[N] = {
-    Nil
+    var distances = nodesByDepth.map((_, Double.PositiveInfinity)).toMap + (from -> 0.0)
+    var spt: Map[N, N] = Map()
+
+    while (!distances.isEmpty) {
+      // search for min in distances
+      // remove min from distances
+
+      // for all succsesors of mininum update its distance v--e--u
+      // if dist(v) + e < dist(u)
+      // dist(u) = dist(v) + e
+      // spt + (u -> v)
+      // v - is min
+
+      val (v, _) = distances.minBy(_._2)
+      distances = distances - v
+
+      val vv = hop(v).get
+      for (e <- vv.outEdges) {
+        val u = e.target.value
+        if (distances(u) < distances(v) + e.value) {
+          distances = distances + (u -> (distances(v) + e.value))
+          spt = spt + (u -> v)
+        }
+      }
+    }
+
+    var step = to
+    var path = List(step)
+    while (spt.contains(step)) {
+      step = spt(step)
+      path = step :: path
+    }
+
+    path.reverse
   }
 }
 
