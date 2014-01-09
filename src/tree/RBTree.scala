25f7ee919032d277a7c6b8957cd6dd1723be8661
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2014-01-10 00:02:48 +0700

    refactored RB tree implementation

--- src/tree/RBTree.scala
+++ src/tree/RBTree.scala
@@ -55,9 +55,24 @@
  *
  * PS: I would be happy, if someone decided to add Matt's 'remove()' method into this
  *     implementation.
- */ 
+ */
 
-abstract class RBTree[+A <% Ordered[A]] {
+/** 
+ * A color for RB-Tree's nodes. 
+ */
+abstract sealed class Color
+case object Red extends Color
+case object Black extends Color
+
+/**
+ * A Red-Black Tree.
+ */
+abstract sealed class Tree[+A <% Ordered[A]] {
+
+  /**
+   * The color of this tree.
+   */
+  def color: Color
 
   /**
    * The value of this tree.
@@ -67,12 +82,12 @@ abstract class RBTree[+A <% Ordered[A]] {
   /**
    * The left child of this tree.
    */
-  def left: RBTree[A]
+  def left: Tree[A]
 
   /**
    * The right child of this tree.
    */
-  def right: RBTree[A]
+  def right: Tree[A]
 
   /**
    * Checks whether this tree is empty or not.
@@ -80,125 +95,67 @@ abstract class RBTree[+A <% Ordered[A]] {
   def isEmpty: Boolean
 
   /**
-   * Checks whether this tree is black or not.
-   */
-  def isBlack: Boolean 
-
-  /**
-   * Checks whether this tree is red or not.
-   */
-  def isRed: Boolean = !isBlack
-
-  /**
    * Adds given element 'x' into this tree.
    *
-   * Time - O(log n)
-   * Space - O(log n)
-   */
-  def add[B >: A <% Ordered[B]](x: B): RBTree[B] = balancedAdd(x).blacken
-
-  /**
-   * Checks whether this tree contains element 'x' or not.
-   *
-   * Time - O(n)
-   * Space - O(n)
-   */
-  def contains[B >: A <% Ordered[B]](x: B): Boolean = 
-    if (isEmpty) false
-    else if (x < value) left.contains(x)
-    else if (x > value) right.contains(x)
-    else true
-
-  /**
-   * Balanced version of "add" method.
+   * Exercise 3.10a @ PFDS.
    *
    * Time - O(log n)
    * Space - O(log n)
    */
-  private def balancedAdd[B >: A <% Ordered[B]](x: B): RBTree[B] = {
-    def balance[B >: A <% Ordered[B]](t: RBTree[B], l: RBTree[B], r: RBTree[B]): RBTree[B] =
-      if (!t.isEmpty && t.isBlack)
-        if (!l.isEmpty && l.isRed)
-          if (!l.left.isEmpty && l.left.isRed) 
-            RedTree(l.value, BlackTree(l.left.value, l.left.left, l.left.right), BlackTree(t.value, l.right, r))
-          else if (!l.right.isEmpty && l.right.isRed) 
-            RedTree(l.right.value, BlackTree(l.value, l.left, l.right.left), BlackTree(t.value, l.right.right, r))
-          else mkTree(t, l, r)
-        else if (!r.isEmpty && r.isRed)
-          if (!r.left.isEmpty && r.left.isRed) 
-            RedTree(r.left.value, BlackTree(t.value, l, r.left.left), BlackTree(r.value, r.left.right, r.right))
-          else if (!r.right.isEmpty && r.right.isRed) 
-            RedTree(r.value, BlackTree(t.value, l, r.left), BlackTree(r.right.value, r.right.left, r.right.right))
-          else mkTree(t, l, r)
-        else mkTree(t, l, r)
-      else mkTree(t, l, r)
+  def add[B >: A <% Ordered[B]](x: B): Tree[B] = {
+    def balancedAdd(t: Tree[A]): Tree[B] =
+      if (t.isEmpty) Tree.make(Red, x)
+      else if (x < t.value) balanceLeft(t.color, t.value, balancedAdd(t.left), t.right)
+      else if (x > t.value) balanceRight(t.color, t.value, t.left, balancedAdd(t.right))
+      else t
 
-    if (isEmpty) RedTree(x)
-    else if (x < value) balance(this, left.balancedAdd(x), right)
-    else if (x > value) balance(this, left, right.balancedAdd(x))
-    else this
+    def balanceLeft(c: Color, x: A, l: Tree[B], r: Tree[A]) = (c, l, r) match {
+      case (Black, Branch(Red, y, Branch(Red, z, a, b), c), d) => 
+        Tree.make(Red, y, Tree.make(Black, z, a, b), Tree.make(Black, x, c, d))
+      case (Black, Branch(Red, z, a, Branch(Red, y, b, c)), d) => 
+        Tree.make(Red, y, Tree.make(Black, z, a, b), Tree.make(Black, x, c, d))
+      case _ => Tree.make(c, x, l, r)
+    }
+
+    def balanceRight(c: Color, x: A, l: Tree[A], r: Tree[B]) = (c, l, r) match {
+      case (Black, a, Branch(Red, y, b, Branch(Red, z, c, d))) =>
+        Tree.make(Red, y, Tree.make(Black, x, a, b), Tree.make(Black, z, c, d))
+      case (Black, a, Branch(Red, z, Branch(Red, y, b, c), d)) => 
+        Tree.make(Red, y, Tree.make(Black, x, a, b), Tree.make(Black, z, c, d))
+      case _ => Tree.make(c, x, l, r)
+    }
+
+    def blacken(t: Tree[B]) = Tree.make(Black, t.value, t.left, t.right)
+
+    blacken(balancedAdd(this))
   }
 
-  /**
-   * Converts this node into black one.
-   *
-   * Time - O(1)
-   * Space - O(1)
-   */
-  private def blacken: RBTree[A] = 
-    if (isBlack) this
-    else BlackTree(value, left, right)
+  def height: Int =
+    if (isEmpty) 0
+    else math.max(left.height, right.height) + 1
 
   /**
-   * Copies a given tree 't' and children 'l' and 'r' into new node.
-   *
-   * Time - O(1)
-   * Space - O(1)
+   * Fails with given message.
    */
-  private def mkTree[B >: A <% Ordered[B]](t: RBTree[B], l: RBTree[B], r: RBTree[B]): RBTree[B] = 
-    if (t.isBlack) BlackTree(t.value, l, r)
-    else RedTree(t.value, l, r)
-
-  /**
-   * Converts this tree into the string representation.
-   *
-   * Time - O(n)
-   * Space - O(log n)
-   */
-  override def toString(): String = 
-    if (isEmpty) "."
-    else if (isRed) "r{" + left + value + right + "}"
-    else "b{" + left + value + right + "}"
+  def fail(m: String) = throw new NoSuchElementException(m)
 }
 
-class RedNode[A <% Ordered[A]](v: A, l: RBTree[A] = Leaf, r: RBTree[A] = Leaf) extends RBTree[A] {
-  def value: A = v
-  def left: RBTree[A] = l
-  def right: RBTree[A] = r
-
+case class Branch[A <% Ordered[A]](color: Color, 
+                              value: A, 
+                              left: Tree[A], 
+                              right: Tree[A]) extends Tree[A] {
   def isEmpty = false
-  def isBlack = false
 }
 
-class BlackNode[A <% Ordered[A]](v: A, l: RBTree[A] = Leaf, r: RBTree[A] = Leaf) extends RBTree[A] {
-  def value: A = v
-  def left: RBTree[A] = l
-  def right: RBTree[A] = r
-
-  def isEmpty = false
-  def isBlack = true
-}
-
-object Leaf extends RBTree[Nothing] {
-  def value: Nothing = throw new NoSuchElementException("Leaf.value")
-  def left: RBTree[Nothing] = throw new NoSuchElementException("Leaf.left")
-  def right: RBTree[Nothing] = throw new NoSuchElementException("Leaf.right")
-
+case object Leaf extends Tree[Nothing] {
+  def color: Color = Black
+  def value: Nothing = fail("An empty tree.")
+  def left: Tree[Nothing] = fail("An empty tree.")
+  def right: Tree[Nothing] = fail("An empty tree.")
   def isEmpty = true
-  def isBlack = true
 }
 
-object RBTree {
+object Tree {
 
   /**
    * Returns an empty red-black tree instance.
@@ -206,7 +163,13 @@ object RBTree {
    * Time - O(1)
    * Space - O(1)
    */
-   def empty[A]: RBTree[A] = Leaf
+   def empty[A]: Tree[A] = Leaf
+
+   /**
+    *
+    */
+   def make[A <% Ordered[A]](c: Color, x: A, l: Tree[A] = Leaf, r: Tree[A] = Leaf): Tree[A] =
+     Branch(c, x, l, r)
 
   /**
    * Creates a new red-black tree from given 'xs' sequence.
@@ -214,31 +177,9 @@ object RBTree {
    * Time - O(n log n)
    * Space - O(log n)
    */
-  def apply[A <% Ordered[A]](xs: A*): RBTree[A] = {
-    var r: RBTree[A] = Leaf
+  def apply[A <% Ordered[A]](xs: A*): Tree[A] = {
+    var r: Tree[A] = Leaf
     for (x <- xs) r = r.add(x)
     r
   }
-}
-
-object RedTree {
-
-  /**
-   * Creates a new red-tree with given element 'x' and children 'l' and 'r'.
-   *
-   * Time - O(1)
-   * Space - O(1)
-   */
-  def apply[A <% Ordered[A]](x: A, l: RBTree[A] = Leaf, r: RBTree[A] = Leaf): RBTree[A] = new RedNode(x, l, r)
-}
-
-object BlackTree {
-
-  /**
-   * Creates a new black-tree with given element 'x' and children 'l' and 'r'.
-   *
-   * Time - O(1)
-   * Space - O(1)
-   */
-  def apply[A <% Ordered[A]](x: A, l: RBTree[A] = Leaf, r: RBTree[A] = Leaf): RBTree[A] = new BlackNode(x, l, r)
 }
